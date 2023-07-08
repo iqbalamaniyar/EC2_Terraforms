e7ff0a365e316700e41136e833556b579054accf
naseer <naseernaaz281@gmail.com>
2023-07-08 13:12:38 +0530

    add

---
+++ main.tf
@@ -0,0 +1,33 @@
+  resource "aws_instance" "webster" {
+    ami = var.ami-id
+    associate_public_ip_address = true
+    instance_type = "t2.micro"
+    key_name = "my_key_pair"
+    vpc_security_group_ids = [ aws_security_group.web_sg.id]
+    subnet_id = module.vpc.public_subnets[0]
+    tags = {
+      "Name" = "web"
+    }
+    depends_on = [
+      module.vpc,
+      aws_security_group.web_sg
+    ]
+
+
+  provisioner "remote-exec" {
+
+      connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = file("~/.ssh/id_rsa")
+        host = aws_instance.webster.public_ip 
+      }
+
+      inline = [
+        "sudo apt update",
+        "sudo apt install nginx -y"
+      ]
+
+    }
+
+}
